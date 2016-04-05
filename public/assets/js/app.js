dbe29078cb47e740505511200c9d7b4d3045f7f3
Byron Wall <byrondevonwall@gmail.com>
2016-04-05 14:37:05 -0400

    add public folder

---
+++ public/assets/js/app.js
@@ -0,0 +1,64 @@
+var foodStream = angular.module("foodStream", ['ngRoute']);
+
+foodStream.config(function($routeProvider){
+  $routeProvider
+    .when('/landing', {
+      templateUrl : '/views/login.html',
+      controller : 'loginController'
+    })
+
+    .when('/signUp', {
+      templateUrl : '/views/signup.html',
+      controller : 'signUpController'
+    })
+
+    .when('/editProfile', {
+      templateUrl : '/views/editprofile.html',
+      controller : 'editProfileController'
+    })
+
+    .when('/create', {
+      templateUrl : '/views/create.html',
+      controller : 'createController'
+    })
+
+    .when('/created', {
+      templateUrl : '/views/created.html',
+      controller : 'createdController'
+    })
+
+    .when('/home', {
+      templateUrl : '/views/home.html',
+      controller : 'homeController'
+    })
+
+    .when('/details', {
+      templateUrl : '/views/postdetail.html',
+      controller : 'detailController'
+    })
+
+    .when('/results', {
+      templateUrl : '/views/searchresults.html',
+      controller : 'resultsController'
+    })
+
+    .when('/claimed', {
+      templateUrl : '/views/claimedpost.html',
+      controller : 'claimedController'
+    })
+
+    .when('/profile', {
+      templateUrl : '/views/profile.html',
+      controller : 'profileController'
+    })
+
+    .when('/chat', {
+      templateUrl : '/views/chat.html',
+      controller: 'chatController'
+    })
+
+    .otherwise({
+      redirectTo: '/landing'
+    });
+
+});
