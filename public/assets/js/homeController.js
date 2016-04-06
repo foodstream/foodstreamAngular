a279f3879acbd16b1b8f1bb69009bb4dd334b897
Byron Wall <byrondevonwall@gmail.com>
2016-04-06 16:10:27 -0400

    ng-repeat on home = the hotness

--- public/assets/js/homeController.js
+++ public/assets/js/homeController.js
@@ -1,4 +1,14 @@
 foodStream.controller('homeController', ['$http', '$scope', '$location', function($http, $scope, $location){
   var token = localStorage.getItem('token')
   console.log(token);
+$scope.posts;
+  $http({
+    method: 'GET',
+    url:' https://sheltered-wildwood-38449.herokuapp.com/posts?token='+token
+  }).then(function successCallback(response){
+    // console.log(response.data);
+    $scope.posts = response.data;
+  }, function errorCallback(response){console.log('hate')
+});
+
 }])
