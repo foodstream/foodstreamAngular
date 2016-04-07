ffa78a9acd8123159c95a92a44cf466d64797e21
Byron Wall <byrondevonwall@gmail.com>
2016-04-06 20:31:06 -0400

    add json data/angular binding to postdetail

--- public/assets/js/detailController.js
+++ public/assets/js/detailController.js
@@ -1,6 +1,22 @@
-6:03]
-detailController
-foodStream.controller('detailController', ['$http', '$scope', 'getPostDetail', function($http, $scope, getPostDetail) {
-   console.log("detail ctrllr up")
-   $scope.detailed = getPostDetail.myFunc();
+foodStream.controller('detailController', ['$http', '$scope', 'getPostDetail', '$location', function($http, $scope, getPostDetail, $location) {
+   console.log("detail ctrllr up");
+  //  console.log(getPostDetail.clickedPost);
+  var postId = localStorage.getItem('postId');
+  var token = localStorage.getItem('token');
+  console.log(postId);
+  localStorage.removeItem('postId');
+
+  $http({
+    method: 'GET',
+    url:' https://sheltered-wildwood-38449.herokuapp.com/posts/'+postId+'.json?token='+token
+  }).then(function successCallback(response){
+    console.log(response.data);
+    $scope.post = response.data;
+    console.log($scope.post.location);
+  }, function errorCallback(response){console.log('hate')
+});
+
+   $scope.goHome = function(){
+     $location.path('/home');
+   }
 }]);
