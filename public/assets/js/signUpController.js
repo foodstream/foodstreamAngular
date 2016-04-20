0573cd74d642a11a861e5627324603900e9bffdf
Byron Wall <byrondevonwall@gmail.com>
2016-04-19 20:28:14 -0400

    add images, style stuff

--- public/assets/js/signUpController.js
+++ public/assets/js/signUpController.js
@@ -16,10 +16,12 @@ foodStream.controller('signUpController', ['$http', '$scope', '$location', funct
       url: "https://sheltered-wildwood-38449.herokuapp.com/users?user[email]=" + $scope.email + "&user[password]=" + $scope.password + "&user[organization]=" + $scope.organization
     }).then(function successCallback(response) {
           //go back to login page and wait for email
+          alert('you have been signed up, please check your email to confirm your address')
           $location.path('/landing');
       }, function errorCallback(response) {
           console.log(response);
           console.log('user not posted');
+          alert('That email address is invalid or already taken, please try again')
       });
 
   };//close submit function
